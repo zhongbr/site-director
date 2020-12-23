3bae4520195630cb6a46d0f6dbb4b97f8549e03e
zhongbr <1178395080@qq.com>
2020-12-23 20:44:22 +0800

    支持csdn

--- src/websites/csdn.js
+++ src/websites/csdn.js
@@ -5,8 +5,7 @@ import {log} from '../utils/log';
 
 export const CSDNDirector = {
     regexp: /^https?:\/\/blog.csdn.net/g,
-    article: "",
-    director(document, window){
+    director(document){
         log('csdn site matched .');
         document.querySelector('#mainBox > main > div.blog-content-box > article').querySelectorAll('a:not([name])').forEach(e=>{
             let a = document.createElement("a");
